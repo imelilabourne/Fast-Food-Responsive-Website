7b331779aafd91e773c9ed32928f5ba8f8d7ae32
Elily <elilabourne.detorres@gmail.com>
2020-05-15 23:42:21 +0800

    added navbar styles

---
+++ burger.js
@@ -0,0 +1,10 @@
+const navOpen = () => {
+    const burger = document.querySelector('.nav2');
+    const navLinks = document.querySelector('nav-links');
+
+    burger.addEventListener('click', () => {
+        navLinks.classList.toggle('nav-active');
+    });
+}
+
+navOpen();

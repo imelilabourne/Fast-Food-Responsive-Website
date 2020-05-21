98c14bb868576801eb13e3f4dc5f1a63ad6e3bc1
Elily <elilabourne.detorres@gmail.com>
2020-05-21 16:53:25 +0800

    created new template, modified style

--- burger.js
+++ burger.js
@@ -1,6 +1,6 @@
 const navOpen = () => {
     const burger = document.querySelector('.nav2');
-    const navLinks = document.querySelector('nav-links');
+    const navLinks = document.querySelector('.nav-links');
 
     burger.addEventListener('click', () => {
         navLinks.classList.toggle('nav-active');
